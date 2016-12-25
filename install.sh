03ef3ed9da860c43b510b007533b239a92d34fc2
Steve28 <steve.sell@gmail.com>
2016-12-24 18:43:20 -0800

    added reading of zone and sources from files

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cp mca66.py /usr/local/bin/mca66_server/
+cp mca66_server.py /usr/local/bin/mca66_server/
+cp zones_list.txt /usr/local/bin/mca66_server/
+cp sources_list.txt /usr/local/bin/mca66_server/
+
+

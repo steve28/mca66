961ad50515b92fd2660a9bc66a48ce251ed85bde
Steve28 <steve.sell@gmail.com>
2018-05-06 09:25:38 -0700

    went back to all GET requests

--- mca66_server.py
+++ mca66_server.py
@@ -2,23 +2,24 @@
 
 from __future__ import print_function
 
-import web, mca66, sys, json
+import web, mca66, sys, json, time
 # Open the serial port to communicate with the MCA-66
 # Exit if it fails.
 
 # Defults
 logfile = open('/var/log/mca66_server/server.log','w',0)
-sys.stdout = logfile
-sys.stderr = logfile
+#sys.stdout = logfile
+#sys.stderr = logfile
 
 audio = mca66.MCA66('/dev/ttyUSB0')
-if audio.open() is False:
-    print("False!!")
-    sys.exit()
+
+# open the audio device - wait until we can reach it.  Most common reason - power is off on the Amp
+while audio.open() is False:
+    print("Could not open MCA-66 comm. Retrying in 15s...")
+    time.sleep(15)
 
 # define list of commands we handle
-post_commands = ['allon','alloff','pwr','volup','voldwn','setvol','setinput','togglemute']
-get_commands = ['status','getzonelabels']
+commands = ['allon','alloff','pwr','volup','voldwn','setvol','setinput','togglemute','status','getzonelabels']
             
 
 urls = (
@@ -39,50 +40,24 @@ class controller:
         # Grab the arguements from the URL
         user_data = web.input()
         print("user_data:",user_data)
-        #print user_data, commands
+        
         # do some input validation
         if 'command' in user_data:
             command = user_data.command.lower()
-            if command in get_commands: 
+            if command in commands: 
                 print("Processing...",command)
                 zone = int(user_data.zone) if 'zone' in user_data else None
-                
+                value = int(user_data.value) if 'value' in user_data else None
+
                 # Process the commnads
                 if command == "status":
                     if zone:
                         audio.queryZone(zone)
                     else:
                         print("Error.")
-                elif command == "getzonelabels":
-                    return json.dumps(audio.getZoneNames())
-            else:
-                print("Invalid command specified:", command)
-        else:
-            print("No command specified")
-             
-        return json.dumps(audio.status())
-    
-    def POST(self):
-        # Grab the arguements from the URL
-        user_data = web.input()
-        # we're going to return JSON
-        web.header('Content-Type', 'application/json')
-        
-        print("Received POST")
-        print("user_data:",user_data)
-        print("data:", web.data())
-        if 'command' in user_data:
-            command = user_data.command.lower()
-            if command in post_commands: 
-                print("Processing...",command)
-                zone = int(user_data.zone) if 'zone' in user_data else None
-                value = int(user_data.value) if 'value' in user_data else None
-            
-                # Process the commnads
-                if command == "allon":
+                elif command == "allon":
                     audio.setPower(0, 1)
                 elif command == "alloff":
-                    #print "AllOff!!!!"
                     audio.setPower(0, 0)
                 elif command == "pwr":
                     if zone and value is not None:
@@ -114,12 +89,18 @@ class controller:
                         audio.toggleMute(zone)
                     else:
                         print("Error.")
+                elif command == "getzonelabels":
+                    return json.dumps(audio.getZoneNames())
             else:
                 print("Invalid command specified:", command)
         else:
             print("No command specified")
-             
+
+        # give the Amp time to reply 
+        time.sleep(0.5)
+        
         return json.dumps(audio.status())
+    
 
 if __name__ == "__main__":
 
